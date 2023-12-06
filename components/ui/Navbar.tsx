444e9cc1adea8a50711e5efde4abfdb9d1010598
Bhavesh Diwale <diwalebhavesh@gmail.com>
2023-12-06 10:23:32 +0530

    Add components

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -7,9 +7,10 @@ export default function () {
         <div>
         <img src="/images/Frame 100.png" alt="" width={40}/>
         </div>
-        <div className="links flex">
-            <Link href='/' className=''>About Us    |  </Link>
-            <Link href='/' className='flex'><img src="/images/Vector.png" alt="contact" className='mx-4'/>Contact Us</Link>
+        <div className="links flex items-center">
+            <Link href='/' className=''>About Us |  </Link>
+            <Link href='/' className='flex w-32 md:me-4'><img src="/images/Vector.png" alt="contact" className='w-6 h-6 mx-2'/>Contact Us</Link>
+            <img src="/images/Avatar.png" alt="" className='md:flex hidden'/>
         </div>
       </div>
     </>
